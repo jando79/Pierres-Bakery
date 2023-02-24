7d0c794cb5bbf59d824c4c594a2eeb8bf94e0fd4
David Jandron <jandron1211@gmail.com>
2023-02-24 09:05:26 -0800

    Passing test for class creation

---
+++ PierresBakery.Tests/ModelsTests/Breads.Tests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PierresBakery.Models;
+//using System.Collections.Generic;
+
+namespace PierresBakery.Tests
+{
+  [TestClass]
+  public class BreadTests
+  {
+     [TestMethod]
+    public void BreadCost_ReturnsCostOfLoaf_Int()
+    {
+
+    }   
+    
+  }
+}
