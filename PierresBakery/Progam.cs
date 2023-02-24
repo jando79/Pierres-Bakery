784fb6c21137884ba307355d9b4149ba2ab6ad56
David Jandron <jandron1211@gmail.com>
2023-02-24 15:46:54 -0800

    WIP User Interface Logic

--- PierresBakery/Progam.cs
+++ PierresBakery/Progam.cs
@@ -10,8 +10,19 @@ namespace UserInterface
      Console.WriteLine("Welcome to Pierre's Bakery");
      Console.WriteLine("We specialize in bread and pastries");
      Console.WriteLine("Bread is $5 per loaf. Buy 2! Get 1 Free!");
-     Console.WriteLine("Pastries $2 each. Buy 3! Get 1 Free!");
-     Console.WriteLine("")
+     Console.WriteLine("Pastries are $2 each. Buy 3! Get 1 Free!");
+
+     Console.WriteLine("How many loaves can we get you?");
+     string stringBreadNumber = Console.ReadLine();
+     Bread newBread = new Bread(stringBreadNumber);
+
+     Console.WriteLine("How many pastires would you like?");
+     string stringPastryNumber = Console.ReadLine();
+     Pastry newPastry = new Pastry(stringPastryNumber);
+
+     Console.WriteLine("The total price of your order is $" + (newBread.BreadPrice()+newPastry.PastryPrice()));
+
+     
     }
   }
 }
