7d2d21ddcea7ea5c76d50e975ced4980df84ff77
David Jandron <jandron1211@gmail.com>
2023-02-24 16:22:45 -0800

    ADDED UI content. Cleaned up code.

--- PierresBakery/Progam.cs
+++ PierresBakery/Progam.cs
@@ -9,7 +9,7 @@ namespace UserInterface
     {
      Console.WriteLine("Welcome to Pierre's Bakery");
      Console.WriteLine("We specialize in bread and pastries");
-     Console.WriteLine("Bread is $5 per loaf. Buy 2! Get 1 Free!");
+     Console.WriteLine("Bread is $5 per loaf. Buy 2! Get 1 Free!"); 
      Console.WriteLine("Pastries are $2 each. Buy 3! Get 1 Free!");
 
      Console.WriteLine("How many loaves can we get you?");
@@ -19,10 +19,11 @@ namespace UserInterface
      Console.WriteLine("How many pastires would you like?");
      string stringPastryNumber = Console.ReadLine();
      Pastry newPastry = new Pastry(stringPastryNumber);
-
-     Console.WriteLine("The total price of your order is $" + (newBread.BreadPrice()+newPastry.PastryPrice()));
-
-     
+  
+     Console.WriteLine("Your total is $" + (newBread.BreadPrice()+newPastry.PastryPrice()));
+     Console.WriteLine("Thank you for your business!");
+     Console.WriteLine("Goodbye and come again!");
     }
   }
 }
+    
